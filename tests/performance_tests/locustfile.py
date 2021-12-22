5a59ba9374a775b551b3c664886571db43a982f0
Arnaud Gagnieu <arnaud.gagnieu@hotmail.fr>
2021-12-22 17:15:59 +0100

    performance tests ok

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -7,17 +7,18 @@ class ProjectPerfTest(HttpUser):
     @task
     def index(self):
         self.client.get("/")
+    
+    @task
+    def logout(self):
+        self.client.get('/logout')
 
     @task
     def showSummary(self):
         self.client.post("/showSummary", {"email":self.login_email})
 
-    """@task
-    def showsummarytest(self):
-        self.client.post("http://127.0.0.1:5000/showSummary", data=dict(email="kate@shelifts.co.uk"))"""
-
     @task
     def purchasePlaces(self):
-        self.client.post('/purchasePlaces', {"places_required":"1",
-                                             "club":"Simply Lift",
-                                             "competition":"Fall Classic"})
+        self.client.post('/purchasePlaces', 
+                         data=dict(places='1',
+                                   club="Simply Lift",
+                                   competition="Fall Classic"))
